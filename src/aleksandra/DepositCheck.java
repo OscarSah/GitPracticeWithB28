1954c0317a703bdbaea72284f8e2bccb3586ea1f
faygen <feyzauzunaygen@gmail.com>
2023-01-12 20:44:40 -0500

    test:FA234 updated customer information task

--- src/aleksandra/DepositCheck.java
+++ src/aleksandra/DepositCheck.java
@@ -2,6 +2,7 @@ package aleksandra;
 
 public class DepositCheck {
     public static void main(String[] args) {
-        System.out.println("Iam testing check deposit ");
+        System.out.println("Iam testing checking account Feyza changed something ");
     }
+
 }
