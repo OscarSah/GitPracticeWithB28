431804be44837e3e11ee3abe7f4ad69053e339c6
atalaykavak <atalay80@hotmail.com>
2023-01-12 20:48:48 -0500

    test: AK456 updated AccountsFeature tests

--- src/oscar/AccountsFeature.java
+++ src/oscar/AccountsFeature.java
@@ -2,6 +2,10 @@ package oscar;
 
 public class AccountsFeature {
     public static void main(String[] args) {
-        System.out.println("I am testing accounts login steps and logout process");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Atalay changed");
+        }
+
     }
+
 }
