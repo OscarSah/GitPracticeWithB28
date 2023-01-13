8158a80a81bf2ff55c2a5d46ac643f8736298c0f
OscarSah <oscar@cybertekschool.com>
2023-01-13 04:44:32 +0300

    test: OS234 updated CustomerInformation tests

--- src/atalay/CustomerInformation.java
+++ src/atalay/CustomerInformation.java
@@ -2,7 +2,7 @@ package atalay;
 
 public class CustomerInformation {
     public static void main(String[] args) {
-        System.out.println("Enter customer name and last name");
+        System.out.println("Enter customer name and last name Oscar changed something");
     }
 
 
