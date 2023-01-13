ee33475547bd11ce17a59602df2dc6c7e8d924a2
Chechulin23 <chechulinmail@gmail.com>
2023-01-12 20:44:48 -0500

    Test: SC23 updated Customer Information tests

--- src/daniel/AccountsFeature.java
+++ src/daniel/AccountsFeature.java
@@ -2,7 +2,7 @@ package daniel;
 
 public class AccountsFeature {
     public static void main(String[] args) {
-        System.out.println("I am testing accounts login steps");
+        System.out.println("I am testing accounts login steps incorrect");
     }
 
 }
